1ac207ef7578fc3b6c31929af7b9b213f3035e4f
Giangry02 <92867283+Giangry02@users.noreply.github.com>
2025-12-12 11:40:45 +0100

    Add comment in RouteApplication.java
    Added a comment to the RouteApplication class.

--- src/main/java/spring/RouteApplication.java
+++ src/main/java/spring/RouteApplication.java
@@ -3,6 +3,7 @@ package spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Ciao
 @SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration.class, org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration.class})
 public class RouteApplication {
     public static void main(String[] args) {
